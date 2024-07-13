61499eb3bef4ebdc77aef298c095e429dca50915
Michael <88445485+mjnyland@users.noreply.github.com>
2024-07-13 17:32:19 -0400

    initial commit

---
+++ PuttingPro/PuttingProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuttingProApp.swift
+//  PuttingPro
+//
+//  Created by Michael Nyland on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PuttingProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
